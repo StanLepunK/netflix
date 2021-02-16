135e5b09ef292f4cc296852ff7a75cb82a993e24
Stan le Punk <stanislasiii@gmail.com>
2021-02-16 17:47:57 +0100

    add import react router dom

--- src/containers/jumbotron_container.js
+++ src/containers/jumbotron_container.js
@@ -4,13 +4,16 @@ import Jumbotron from '../components/jumbotron';
 import jumbo_data from '../fixtures/jumbo.json';
 
 export function JumbotronContainer() {
+  let path = `https://raw.githubusercontent.com/StanLepunK/netflix/master`;
   return (
     <Jumbotron.Container>
       {jumbo_data.map((elem) => (
         <Jumbotron key={elem.id} direction={elem.direction}>
           <Jumbotron.Title>{elem.title}</Jumbotron.Title>
           <Jumbotron.SubTitle>{elem.subTitle}</Jumbotron.SubTitle>
-          <Jumbotron.Image src={elem.path_img} alt={elem.alt} />
+          {/* {`mode${modeNumber}`} */}
+          <Jumbotron.Image src={`${path}${elem.path_img}`} alt={elem.alt} />
+          {/* <Jumbotron.Image src=`{${{path}}${elem.path_img}` alt={elem.alt} /> */}
         </Jumbotron>
       ))}
     </Jumbotron.Container>
