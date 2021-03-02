09d061d52eb1aa1050e7e0ff5a8a8953960c75f6
Stan le Punk <stanislasiii@gmail.com>
2021-03-02 12:23:50 +0100

    add logo version

--- src/containers/profiles_container.js
+++ src/containers/profiles_container.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Header } from '../components/h';
+import * as ROUTES from '../constants/routes_constants';
+import { MEDIA_PATH } from '../constants/constants.js';
 
 export function SelectProfileContainer() {
-  return <p>Profile container</p>;
+  return (
+    <>
+      <Header bg={false}>
+        <Header.Frame>
+          <Header.Logo
+            to={ROUTES.HOME}
+            src={`${MEDIA_PATH}/images/misc/logo.svg`}
+            alt="Netflix"
+          />
+        </Header.Frame>
+      </Header>
+    </>
+  );
 }
